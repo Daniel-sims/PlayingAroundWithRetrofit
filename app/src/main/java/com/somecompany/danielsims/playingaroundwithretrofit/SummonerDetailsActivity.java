54f2ff1ae8c64871c1992811c2aa68af0c354192
Daniel <daniel.sims1994@outlook.com>
2018-05-04 08:25:42 +0100

    Recent matches now show in the recyclerview in basic format

--- app/src/main/java/com/somecompany/danielsims/playingaroundwithretrofit/SummonerDetailsActivity.java
+++ app/src/main/java/com/somecompany/danielsims/playingaroundwithretrofit/SummonerDetailsActivity.java
@@ -1,12 +1,21 @@
 package com.somecompany.danielsims.playingaroundwithretrofit;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.somecompany.danielsims.playingaroundwithretrofit.Models.Match;
 import com.somecompany.danielsims.playingaroundwithretrofit.Models.MatchList;
 import com.somecompany.danielsims.playingaroundwithretrofit.Models.Summoner;
 import com.somecompany.danielsims.playingaroundwithretrofit.Models.SummonerRank;
+
+import java.util.List;
 
 public class SummonerDetailsActivity extends AppCompatActivity {
 
@@ -33,6 +42,8 @@ public class SummonerDetailsActivity extends AppCompatActivity {
         mFoundSummonerDivision = findViewById(R.id.FoundSummonerDivision);
         mFoundSummonerLp = findViewById(R.id.FoundSummonerLp);
 
+        RecyclerView matchRecyclerView = findViewById(R.id.matchlist_recyclerview);
+
         mSummoner = getIntent().getParcelableExtra("summoner");
         mSummonerRank = getIntent().getParcelableExtra("summonerrank");
         mMatchList = getIntent().getParcelableExtra("matchlist");
@@ -44,6 +55,59 @@ public class SummonerDetailsActivity extends AppCompatActivity {
             mFoundSummonerTier.setText(mSummonerRank.getTier());
             mFoundSummonerDivision.setText(mSummonerRank.getRank());
             mFoundSummonerLp.setText(mSummonerRank.getLeaguePoints().toString());
+
+            MatchListAdapter matchListAdapter = new MatchListAdapter(mMatchList.getMatches());
+            matchRecyclerView.setAdapter(matchListAdapter);
+            matchRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         }
     }
+
+    //https://guides.codepath.com/android/using-the-recyclerview
+    public class MatchListAdapter extends RecyclerView.Adapter<SummonerDetailsActivity.MatchListAdapter.ViewHolder> {
+        public List<Match> mMatches;
+
+        public MatchListAdapter(List<Match> matches){
+            mMatches = matches;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            Context context = parent.getContext();
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+            View matchView = layoutInflater.inflate(R.layout.recycler_view_match, parent, false);
+            ViewHolder viewHolder = new ViewHolder(matchView);
+
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            Match match = mMatches.get(position);
+
+            holder.mChampionId.setText(match.getChampion().toString());
+            holder.mUserRole.setText(match.getRole());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mMatches.size();
+        }
+
+
+        public class ViewHolder extends RecyclerView.ViewHolder{
+            public TextView mChampionId;
+            public TextView mUserRole;
+
+            public ViewHolder(View itemView){
+                super(itemView);
+
+                mChampionId = itemView.findViewById(R.id.champion_id);
+                mUserRole = itemView.findViewById(R.id.user_role);
+            }
+        }
+
+    }
+
+
 }
